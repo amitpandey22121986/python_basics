1020938b31dfe4905af1fda32789df2c6211292b
amitpandey22121986 <amitpandey22121986@gmail.com>
2024-02-12 12:44:10 +0530

    adding basic python project in git

---
+++ numeric-types.py
@@ -0,0 +1,7 @@
+'''Data types of a variable are interpreted at runtime and we don't have to initially declare it,
+that is why Python is called dynamically-typed language'''
+
+a = int(input())
+b = int(input())
+
+print(a+b)
